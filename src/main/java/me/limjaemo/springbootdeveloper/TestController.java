ce4c271791a03deccec67471dd6e44597a363a47
JAEMO-LIM <tjdwkddl@gmail.com>
2026-01-19 21:19:56 +0900

    Project origin File

---
+++ src/main/java/me/limjaemo/springbootdeveloper/TestController.java
@@ -0,0 +1,12 @@
+package me.limjaemo.springbootdeveloper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "<h1>빡집중 ㄱㄱ</h1>";
+    }
+}
